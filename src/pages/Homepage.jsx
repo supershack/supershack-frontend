9b43fe690ef769b88759a753375365a87ee6f5c1
NISHA PATIL <prashantpatil@prashants-air.kpn>
2024-03-12 15:00:16 +0100

    homepage has been added and display upload image button

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,13 @@
-function Homepage() {
-    return (
-        <div>Homepage</div>
-    )
+import React from 'react'
+
+const Homepage = () => {
+  return (
+   <>
+   <h1>Guess Your Job Match</h1>
+    <div className='image'></div>
+    <button id='image'>Upload Your Photo</button>
+    </>
+  )
 }
 
 export default Homepage
