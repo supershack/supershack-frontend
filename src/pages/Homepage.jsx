54139d199aa3e6be7dc515e0dfb5ebc799bddf8e
NISHA PATIL <prashantpatil@prashants-air.kpn>
2024-03-13 12:22:23 +0100

    functionality for uploading images has been added

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 const Homepage = () => {
-  return (
-   <>
-   <h1>Guess Your Job Match</h1>
-    <div className='image'></div>
-    <button id='image'>Upload Your Photo</button>
-    </>
-  )
-}
+  const [file, setFile] = useState(null);
 
-export default Homepage
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    axios
+      .post("http://localhost:3000/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Guess Your Job Match</h1>
+        <form className='image' onSubmit={handleSubmit}>
+          <input id='input' type="file" onChange={handleFileChange} />
+          <button id='image' type="submit">Upload</button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Homepage;
