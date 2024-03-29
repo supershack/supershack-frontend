2fe94abc697f77e2fbd3fc8b47e30e38308c4169
Noah Raissi <coding@noahraissi.com>
2024-03-29 21:04:41 +0100

    Feature/homescreen new (#3)
    * adding flower files and helpers
    
    * Working flower
    
    * adjusted sizing with screenwidth

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
+//import Flower from "../components/Flower";
+import CanvasComponent from '../components/flower/CanvasComponent';
 
 const Homepage = () => {
   return (
     <>
       <div>
+      <CanvasComponent /> 
         <h1>Guess Your Job Match</h1>
         <Link to={`/upload`}>Upload Image</Link>
       </div>
