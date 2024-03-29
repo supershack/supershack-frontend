0c9cfb8438c475740634105dfbf336db09874e33
Noah Raissi <coding@noahraissi.com>
2024-03-29 12:30:41 +0100

    adding flower files and helpers

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import Flower from "../components/Flower";
 
 const Homepage = () => {
   return (
     <>
       <div>
+      <Flower /> 
         <h1>Guess Your Job Match</h1>
         <Link to={`/upload`}>Upload Image</Link>
       </div>
