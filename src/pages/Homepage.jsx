9d23d8e705fbbb00b38dbbd82db48e04aa812143
LM-Vasco <147736531+LM-Vasco@users.noreply.github.com>
2024-04-01 20:23:52 +0100

    File Upload Page
    * added gif as background image
    
    * added the other two image objects
    
    * Might need some adjusting in css to media screens, but I've been always working in responsive mode with several devices options

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 //import Flower from "../components/Flower";
-import CanvasComponent from '../components/Flower/CanvasComponent';
+import CanvasComponent from '../components/flower/CanvasComponent';
 
 const Homepage = () => {
   return (
     <>
-      <div className="collectorCard-page">
+      <div className="defaultPage-container">
         <CanvasComponent />
         <Link to={`/upload`}>
-          <button className="buttonDefault starten">Starten</button>
+          <button className="buttonDefault">Starten</button>
         </Link>
       </div>
     </>
